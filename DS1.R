69f14dfc45768e9682203cd8a2250c50934fbc55
Manasi-Sanju-Chavan <87053557+Manasi-Sanju-Chavan@users.noreply.github.com>
2023-09-02 11:34:53 +0530

    Add files via upload

---
+++ DS1.R
@@ -0,0 +1,31 @@
+print('Learning R')
+print (10)
+print(10*5)
+a <- 10*5
+a
+ten = 10L
+class(ten)
+city = "pune"
+class(city)
+pincode = "83891"
+class(pincode)
+boolean = TRUE
+class(boolean)
+rm(city)
+age = 20,21,24,25,26
+
+# homogeneous DS (vector)
+age = c(20,21,22,23,24)
+city = c("Mumbai","Pune","Delhi","Kolkata")
+info = c("sakshi",25,150.6,TRUE)
+info1 = c(25,"Sakshi",150.6,TRUE)
+print(info[2])
+age[2]
+age[1:3]
+info[2:3]
+info[c(1,3)]
+age[c(1,4,5)]
+info[2] = '22' #replacing values
+info
+info[c(1,4)] = c("Tarun","FALSE")
+info
